83f995956b1247dd865f3264fb8d191cb8e641a9
Hayden Bradfield <me@haydenbradfield.com>
2022-10-06 23:31:20 -0400

    fix config file

--- config/index.js
+++ config/index.js
@@ -1,9 +1,7 @@
 import * as dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({
-    path: path.resolve(process.cwd(), '..')
-})
+dotenv.config()
 
 export default {
     env: process.env.NODE_ENV || 'development', 
