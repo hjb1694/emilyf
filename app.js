2e42c46daf347ba7dec60478e2acd73589f725ff
Hayden Bradfield <me@haydenbradfield.com>
2022-10-06 23:29:44 -0400

    add nodemon; other things

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import config from './config/index.js';
 import express from 'express';
 import path from 'path';
+import allRoutes from './routes/all.routes.js'
 
 const app = express();
 app.use(express.static(path.join(process.cwd(), 'assets')));
 app.set('view-engine', 'ejs');
 app.set('views', './views');
+
+//Routes 
+app.use(allRoutes);
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`))
---
+++ routes/all.routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => { 
+    res.render('index.ejs');
+});
+
+
+export default router;
