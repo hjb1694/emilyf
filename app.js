c54eb3611f66baaad27134ed8872247b498ad3c3
Hayden Bradfield <me@haydenbradfield.com>
2022-10-12 20:07:12 -0400

    wip

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ import helmet from 'helmet';
 import compression from 'compression';
 
 const app = express();
-//app.use(helmet());
+app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), 'assets')));
 app.use(session({
@@ -15,7 +16,6 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
-app.use(compression());
 app.set('view-engine', 'ejs');
 app.set('views', './views');
 
