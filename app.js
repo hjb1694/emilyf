30dc9b8c2dff9e17d783bd5ef826e58ee9bd53eb
Hayden Bradfield <me@haydenbradfield.com>
2022-10-12 21:08:54 -0400

    wip

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import express from 'express';
 import path from 'path';
 import allRoutes from './routes/all.routes.js'
 import session from 'express-session';
-import helmet from 'helmet';
-import compression from 'compression';
+// import helmet from 'helmet';
+// import compression from 'compression';
 
 const app = express();
-app.use(helmet());
+// app.use(helmet());
 // app.use(compression());
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), 'assets')));
