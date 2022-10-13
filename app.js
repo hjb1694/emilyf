985bcedda83fbb6fe6b8015498e38a52a4a2521c
Hayden Bradfield <me@haydenbradfield.com>
2022-10-12 21:06:27 -0400

    wip

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import compression from 'compression';
 
 const app = express();
 app.use(helmet());
-app.use(compression());
+// app.use(compression());
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), 'assets')));
 app.use(session({
